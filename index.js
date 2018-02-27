d793ded3181367655046b53ecc5bc88e5d62de1f
John Nenniger <JPNENNIGER@GMAIL.com>
2018-02-27 14:36:24 -0800

    no logs

--- index.js
+++ index.js
@@ -42,18 +42,17 @@ app.post('/start', (request, response) => {
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
-  // NOTE: Do something here to generate your move
   // console.log(`my health:  ${request.body.you.health}`)
   // console.log(`turn number: ${request.body.turn}`)
   // request.body.snakes.data[0].body.data.forEach((snake) => {
   //   console.log(`a snake: ${snake}`)
   // })
-  request.body.you.body.data[0].forEach((youObj) => {
-    console.log(`me object:${youObj}`)
-  })
-
-  console.log(`food is here: ${request.body.food.data[0]}`)
-  console.log(`this is how long the dumb  ${typeof request.body.food.data[0]}   ${request.body.food.data[0].length}`)
+  // request.body.you.body.data[0].forEach((youObj) => {
+  //   console.log(`me object:${youObj}`)
+  // })
+  //
+  // console.log(`food is here: ${request.body.food.data[0]}`)
+  // console.log(`this is how long the dumb  ${typeof request.body.food.data[0]}   ${request.body.food.data[0].length}`)
   // console.log(`snakes:  ${request.body.snakes.data}`)
   // Response data
   const data = {
