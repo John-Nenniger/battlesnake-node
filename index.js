41d8a158860e044ee937b5acafd37b9db0cf69bf
Keano <keanov@gmail.com>
2018-02-28 15:20:08 -0800

    log

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(poweredByHandler)
 
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
-  console.log(request.body)
+  console.log('game start', request.body)
   // NOTE: Do something here to start the game
 
   // Response data
