83bad76b7fe5cccc64216b2cdc6c807337b7c64d
John Nenniger <JPNENNIGER@GMAIL.com>
2018-02-28 15:14:52 -0800

    generated grid

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const {
   poweredByHandler
 } = require('./handlers.js')
 
+const column = [];
+column[19] = 0;
+column.fill(0);
+// console.log(column, column.length)
+const grid = [];
+for (let x=0;x<20;x++){
+  grid.push(column);
+}
+// console.log(grid)
+
+
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
@@ -23,13 +34,14 @@ app.use(poweredByHandler)
 
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
-  // NOTE: Do something here to start the game
+  const food = [request.body.food.data[0].x, request.body.food.data[0].y]
+  const snekPlace = [request.body.you.body.data[0].x, request.body.you.body.data[0].y]
 
   // Response data
   const data = {
-    color: '#DFFF00',
-    head_url: 'http://www.placecage.com/c/200/200', // optional, but encouraged!
-    taunt: "Let's do thisss thang!", // optional, but encouraged!
+    color: 'teal',
+    head_url: 'http://25.media.tumblr.com/7fcfecd8dba001ecdbae6bff5fe10342/tumblr_mg6awfY46I1s2a05ho1_400.gif', // optional, but encouraged!
+    taunt: "!?", // optional, but encouraged!
   }
 
   return response.json(data)
@@ -38,6 +50,8 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  const food = [request.body.food.data[0].x, request.body.food.data[0].y]
+  const snekPlace = [request.body.you.body.data[0].x, request.body.you.body.data[0].y]
 
   // Response data
   const data = {
