3854b76732a1334b15a00664e3bb4eaf089d1f73
Keano <keanov@gmail.com>
2018-02-28 18:41:35 -0800

    paints around snakes poorly

--- snakes.js
+++ snakes.js
@@ -9,16 +9,24 @@ for (let x = 0; x < 20; x++) {
 }
 
 const paintAroundSnakes = (gameGrid, coords) => {
+    gameGrid[coords.x][coords.y - 1] = -8 // top
+    gameGrid[coords.x + 1][coords.y] = -8 // right
+    gameGrid[coords.x][coords.y + 1] = -8 // down
+    gameGrid[coords.x - 1][coords.y] = -8 // left
 
+    return true;
 }
 
 const updateGridWithSnakes = (gameGrid, snakePositions) => {
     snakePositions.snakes.data.forEach(snake => {
 
         snake.body.data.forEach(coords => {
+            paintAroundSnakes(gameGrid, coords);
             gameGrid[coords.y][coords.x] = -10;
         })
     })
+
+    return true;
 }
 
 updateGridWithSnakes(grid, snek.json)
