2d5685592788cc3725374b3661c015123ad487c8
Keano <keanov@gmail.com>
2018-03-01 17:48:25 -0800

    Add diagonal painting

--- snakes.js
+++ snakes.js
@@ -1,7 +1,8 @@
 const snek = require('./snekJson');
 
-const adjacentToSnakeWeight = -10;
-const snakeWeight = -8;
+const adjacentToSnakeWeight = 2;
+const snakeWeight = 1;
+const radius = 2;
 
 let grid = [];
 for (let x = 0; x < 20; x++) {
@@ -11,21 +12,62 @@ for (let x = 0; x < 20; x++) {
     grid.push(column);
 }
 
-const paintAroundSnakes = (gameGrid, coords) => {
-    if (coords.y - 1 > 0) {
-        gameGrid[coords.y - 1][coords.x] = adjacentToSnakeWeight // TOP
-    }
+const paintAroundSnakes = (gameGrid, coords, distance) => {
+    // Will only update the grid if the coordinates provided 
+    // are within the bounds of the game
+    let d = distance;
 
-    if (coords.y + 1 < gameGrid.length) {
-        gameGrid[coords.y + 1][coords.x] = adjacentToSnakeWeight // DOWN
-    }
+    // d is equal to the 'radius' to paint around paint from the outside in. Stop when d < 0
+    // adjacentToSnakeWeight can also be adjusted on every increment
+    while (d > 0) {
+        const exists = {
+            top: false,
+            down: false,
+            right: false,
+            left: false
+        }
 
-    if (coords.x + 1 < gameGrid.length) {
-        gameGrid[coords.y][coords.x + 1] = adjacentToSnakeWeight // RIGHT
-    }
+        // Check cardinal directions if they are within limit, set exist to true for the diagonal directions
 
-    if (coords.x - 1 > 0) {
-        gameGrid[coords.y][coords.x - 1] = adjacentToSnakeWeight // LEFT
+        if (coords.y - d >= 0) {
+            gameGrid[coords.y - d][coords.x] = adjacentToSnakeWeight; // TOP
+            exists.top = true;
+        }
+
+        if (coords.y + d < gameGrid.length) {
+            gameGrid[coords.y + d][coords.x] = adjacentToSnakeWeight; // DOWN
+            exists.down = true;
+        }
+
+        if (coords.x + d < gameGrid.length) {
+            gameGrid[coords.y][coords.x + d] = adjacentToSnakeWeight; // RIGHT
+            exists.right = true;
+        }
+
+        if (coords.x - d >= 0) {
+            gameGrid[coords.y][coords.x - d] = adjacentToSnakeWeight; // LEFT
+            exists.left = true;
+        }
+
+        // There can only be a diagonal if both cardinal directions exist
+
+        if (exists.top && exists.right) {
+            gameGrid[coords.y - d][coords.x + d] = adjacentToSnakeWeight; // TOP RIGHT
+        }
+
+        if (exists.down && exists.right) {
+            gameGrid[coords.y + d][coords.x + d] = adjacentToSnakeWeight; // DOWN RIGHT
+        }
+
+        if (exists.down && exists.left) {
+            gameGrid[coords.y + d][coords.x - d] = adjacentToSnakeWeight; // DOWN LEFT
+        }
+
+        if (exists.top && exists.right) {
+            gameGrid[coords.y - d][coords.x - d] = adjacentToSnakeWeight; // TOP LEFT
+        }
+
+        d--;
     }
 
     return true;
@@ -35,11 +77,9 @@ const updateGridWithSnakes = (gameGrid, snakePositions) => {
     snakePositions.snakes.data.forEach(snake => {
 
         snake.body.data.forEach(coords => {
-            paintAroundSnakes(gameGrid, coords);
-            // gameGrid[coords.y][coords.x] = snakeWeight;
+            paintAroundSnakes(gameGrid, coords, radius);
         })
         snake.body.data.forEach(coords => {
-            // paintAroundSnakes(gameGrid, coords);
             gameGrid[coords.y][coords.x] = snakeWeight;
         })
     })
@@ -51,5 +91,5 @@ module.exports = updateGridWithSnakes;
 
 /* TESTING */
 
-// updateGridWithSnakes(grid, snek.json)
-// console.log(grid);
+updateGridWithSnakes(grid, snek.json)
+console.log(grid);
