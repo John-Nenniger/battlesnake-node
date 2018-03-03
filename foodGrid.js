98fc3d1844372318471f0e4f1d99fd7dbee69af4
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-03 13:10:20 -0800

    added ability to have multiple food pellets

--- foodGrid.js
+++ foodGrid.js
@@ -6,30 +6,35 @@ function absoluteDifference(a, b) {
   return Math.abs(a - b)
 }
 
-const updateFoodGrid = (x, y, grid, boardLength) => {
-  let minx = x - 5;
-  let maxx = x + 5;
-  let miny = y - 5;
-  let maxy = y + 5;
+const updateFoodGrid = (food, grid, boardLength) => {
+  food.forEach((array) => {
+    console.log(array, array[0], array[1])
+    let x = array[0]
+    let y = array[1]
+    let minx = x - 5;
+    let maxx = x + 5;
+    let miny = y - 5;
+    let maxy = y + 5;
 
-  for (let currentx = minx, counter = 0; currentx <= maxx; currentx++ , counter++) {
-    if (currentx < 0 || currentx > boardLength - 1) {
-      continue
-    }
-    // now I need a number that starts at 1, then goes up to 9 by incrementign by 2
-    // then increments by 2 back down to 1.  Maybe I'll just make it an array?
-    let diamond = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
-    for (let currenty = y - diamond[counter]; currenty <= y + diamond[counter]; currenty++) {
-      if (currenty < 0 || currenty > boardLength) {
+    for (let currentx = minx, counter = 0; currentx <= maxx; currentx++ , counter++) {
+      if (currentx < 0 || currentx > boardLength - 1) {
         continue
       }
-      distance = absoluteDifference(currentx, x) + absoluteDifference(y, currenty)
+    // now I need a number that starts at 1, then goes up to 9 by incrementign by 2
+    // then increments by 2 back down to 1.  Maybe I'll just make it an array?
+      let diamond = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+      for (let currenty = y - diamond[counter]; currenty <= y + diamond[counter]; currenty++) {
+        if (currenty < 0 || currenty > boardLength) {
+          continue
+        }
+        distance = absoluteDifference(currentx, x) + absoluteDifference(y, currenty)
       // console.log(foodValue(distance));
       // console.log(grid[currenty][currentx]);
 
-      grid[currenty][currentx] += foodValue(distance)
+        grid[currenty][currentx] += foodValue(distance)
+      }
     }
-  }
+  })
 }
 
 module.exports = {
--- gridInterpreter.js
+++ gridInterpreter.js
@@ -8,7 +8,7 @@
 function pick(x, y, grid, prevMove) { // where x and y refer to the
   // first we need to remove the opposite of the previous move from the possibilities
   // opposites.prevMove  is the one we can't pick
-  console.log('in pick: ', 'x', x, `y:${y}`)
+  // console.log('in pick: ', 'x', x, `y:${y}`)
   //so the problem is that grid[-1] is undefined, then we try to get a property from it
   // and that crashes... This is what needs to be fixed
   let values = {};
@@ -20,14 +20,14 @@ function pick(x, y, grid, prevMove) { // where x and y refer to the
   } else {
     values = { 'down': grid[y + 1][x], 'up': grid[y - 1][x], 'right': grid[y][x + 1], 'left': grid[y][x - 1] };
   }
-  console.log(values)
+  // console.log(values)
   let opposites = { 'up': 'down', 'right': 'left', 'down': 'up', 'left': 'right' }
   let directions = ['up', 'down', 'left', 'right']
   let greatest = -100;
   let nextMove = '';
   for (let key in values) {
     if (values[key] === undefined || key === opposites[prevMove]) {
-      console.log('continue!')
+      // console.log('continue!')
       continue
     } else if (values[key] > greatest) {
       greatest = values[key]
